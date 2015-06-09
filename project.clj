024711e4aa80370d85a0c2331082052b073df608
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-06-10 00:29:49 +0100

    description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject flowing "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Library for building data-driven workflows (dataflows)"
   :url "https://github.com/stain/flowing-clj"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
