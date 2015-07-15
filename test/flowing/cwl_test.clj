9cfa0709934865ed0b405092924a6af22b778718
Stian Soiland-Reyes <soiland-reyes@cs.manchester.ac.uk>
2015-07-15 10:49:19 +0100

    test resolved absolute URIs

--- test/flowing/cwl_test.clj
+++ test/flowing/cwl_test.clj
@@ -1,6 +1,7 @@
 (ns flowing.cwl-test
   (:require [clojure.test :refer :all]
             [flowing.core :refer :all]
+            [clojure.pprint :refer [pprint]]
             [flowing.cwl :refer :all]
             [clojure.java.io :as io ]
             ))
@@ -8,9 +9,14 @@
 (testing "parse-cwl"
   (let [res (io/resource "example/revsort.cwl")
         wf (parse-cwl res)]
-    (println wf)
+    (pprint wf)
     (is (= "Workflow" (:class wf)))
     ; Check the :import worked
     (is (= "CommandLineTool" (get-in (first (:steps wf)) [:run :class])))
-    
+    ; Check URIs are absolute
+;    (print (:id (first (:outputs wf))))
+    (is (.startsWith (:id (first (:outputs wf))) "file:" ))
+    (is (.endsWith (:id (first (:outputs wf))) "revsort.cwl#output"))
+    ; and different filename in imported blocks
+    (is (.endsWith (:id (first (get-in (first (:steps wf)) [:run :inputs]))) "revtool.cwl#input"))
   ))
